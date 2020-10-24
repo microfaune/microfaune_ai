837d8f10356e726aa4bd353b57a6475a1201fcbf
Alphonse Harouny <wharouny.tp@gmail.com>
2020-10-24 02:16:13 +0200

    evolution - Get the related prediction sequence(int,int) list of the media file positive annotation

--- microfaune/domain/track.py
+++ microfaune/domain/track.py
@@ -1,21 +1,55 @@
 PREDICTION_SAMPLE_WIDTH_IN_MS = 20
 
 class Track:
-    # trackEmlt : dict
-    #
-    # def __init__(self, trackEmlt : dict):
-    #     self.trackEmlt = trackEmlt
 
+    # "tracks": [
+    #     {
+    #         "id": 47,
+    #         "name": "SWIFT_20000101_022052.wav",
+    #         "file": "/media/SWIFT_20000101_022052.wav",
+    #         "format": "wav",
+    #         "project_id": 1,
+    #         "duration": 60.0,
+    #         "prediction": "",
+    #         "annotation_set": [
+    #             {
+    #                 "id": 1,
+    #                 "track_id": 47,
+    #                 "value":"[
+    #                   {\"id\":\"wavesurfer_4kgitqcktig\",\"start\":4.935061842665718,\"end\":10.509955195406347,\"annotation\":\"\"},
+    #                   {\"id\":\"wavesurfer_afb13jpasm8\",\"start\":17.55982033205593,\"end\":22.95971703246838,\"annotation\":\"\"},
+    #                   {\"id\":\"wavesurfer_jdu8bguik4\",\"start\":26.334652470226157,\"end\":30.184578821446145,\"annotation\":\"\"}
+    #                 ]",
+    #           "user_id": 1,
+    #           "reviewed": false,
+    #           "reviewed_by_id": null,
+    #           "date_time": "2020-10-17T19:12:13.800Z",
+    #           "username": "admin",
+    #           "reviewer": null
+    #           }
+    #         ] --> Fin annotation_set
+    #     }, --> Fin du TrackElmt
+    # ] --> Fin du Tracks
 
-    def map_annotation_set_elmt_to_prediction_ndxes(self, annotation_set:dict) -> [(int,int)] :
-        # annotation_set_elmt est une sequence de duration càd de 60 sec.
-        positive_prediction_ndxes = [(int,int)] # (start_poisition_index, end_position_index)
-        positive_prediction_ndxes.append(
-            # annotation_set.get("annotation_set")
-                map(lambda elmt: self.convert_from_annonation_elmt_time_to_prediction_order(elmt.get("start")), annotation_set.get("annotation_set"))
-        )
-        return positive_prediction_ndxes
+    def map_annotation_set_to_prediction_ndxes(self, track_elmt:dict) -> [(int,int)] :
+        # return [ self._map_annotation_set_elmt_to_prediction_ndxes(annotation_set_elmt.get("value"))
+        #          for annotation_set_elmt in track_elmt.get("annotation_set") ]
+        l = [(int,int)]
+        l.append(self._map_annotation_set_elmt_to_prediction_ndxes(annotation_set_elmt.get("value"))
+                 for annotation_set_elmt in track_elmt.get("annotation_set"))
+        return l
 
-    def convert_from_annonation_elmt_time_to_prediction_order(self, start_time_offset_in_sec:float, end_time_offset_in_sec:float) -> (int,int):
-        return ( (int) (start_time_offset_in_sec * 1000 // PREDICTION_SAMPLE_WIDTH_IN_MS) + 1,
+    def _map_annotation_set_elmt_to_prediction_ndxes(self, value_list:[dict]) -> [(int,int)]:
+        l = [(int,int)]
+        l.append(self._convert_from_annonation_elmt_time_to_prediction_order(value.get("start"), value.get("end"))
+                 for value in value_list)
+        return l
+
+    # return [ self._convert_from_annonation_elmt_time_to_prediction_order(value.get("start"), value.get("end"))
+        #          for value in value_list ]
+
+    def _convert_from_annonation_elmt_time_to_prediction_order(self, start_time_offset_in_sec:float, end_time_offset_in_sec:float) -> (int,int):
+        (a,b) = ( (int) (start_time_offset_in_sec * 1000 // PREDICTION_SAMPLE_WIDTH_IN_MS) + 1,
                  (int) (end_time_offset_in_sec * 1000 // PREDICTION_SAMPLE_WIDTH_IN_MS) + 1)
+        print(f'\nstart:{a} - end:{b}')
+        return (a,b)
--- microfaune/validation.py
+++ microfaune/validation.py
@@ -13,24 +13,34 @@ class RNNValidator:
     def load_annotation_file(self, json_file_path :str) -> dict :
         return file_utils.read_json_file(json_file_path)
 
-    def map_annotation_elmt_to_prediction_ndx(self, all_annotations:dict) -> [int] :
-        prediction_ndxes = [(int,int)]
+    def map_annotation_elmt_to_prediction_ndx(self, media_file_annotation:dict) -> [(int,int)] :
+        ''' Get the related prediction sequence(int,int) list of the media file positive annotation
+
+        :param Media_file_annotation: Json representation of the annotated file
+        :return: A List of (int, int) where each tuple represent the sequence of the related prediction 'start index' and 'end index'
+        '''
         track = Track()
-        self.compute_prediction_metrics(
-            # all_annotations.get("tracks")
-                map(lambda annotation_set: track.map_annotation_set_elmt_to_prediction_ndxes(annotation_set), all_annotations.get("tracks"))
-        )
-        return 0
+
+
+        # self.compute_prediction_metrics(
+        #         map(lambda track_elmt: track.map_annotation_set_to_prediction_ndxes(track_elmt), all_annotations.get("tracks"))
+        # )
+        # return [ track.map_annotation_set_to_prediction_ndxes(track_elmt) for track_elmt in media_file_annotation.get("tracks")]
+        l = [(int,int)]
+        l.append(track.map_annotation_set_to_prediction_ndxes(track_elmt) for track_elmt in media_file_annotation.get("tracks"))
+        return l
 
     def compute_prediction_metrics(self, positive_prediction_ndxes = [(int,int)]) -> (float, float, float):
         for biNdx in positive_prediction_ndxes :
-            print(f"start:%d - end:%d", biNdx[0], biNdx[1] )
+            print(f"start:{biNdx[0]} - {biNdx[1]}")
+            # print(f"start:{biNdx[1].__class__}")
         return (1,2,3)
 
 if __name__ == '__main__' :
     detector = RNNDetector()
     validator = RNNValidator(detector)
     all_annotations = validator.load_annotation_file( os.path.abspath(os.path.join(os.path.dirname(__file__), "media-annotation/SWIFT_20000101_022052.json")) )
-    validator.map_annotation_elmt_to_prediction_ndx(all_annotations)
+    list_of_tuples = validator.map_annotation_elmt_to_prediction_ndx(all_annotations)
+    validator.compute_prediction_metrics(list_of_tuples)
     # global_score, local_score = detector.predict_on_wav(os.path.abspath(os.path.join(os.path.dirname(__file__), "media/SWIFT_20190723_050006.wav"))) # NB: Check that loaded wav file actually exists on your disk
     # print(f"Golbal score: {global_score}  -  Localscore: {local_score}")
